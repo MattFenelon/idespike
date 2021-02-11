05e19e9851bf1eeb6f6b4a3c6db795673477219c
Matthew Fenelon <matt@fnln.me>
2021-02-11 16:34:41 +0000

    Add monaco-editor integration

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,78 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useEffect, useState } from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Editor from "@monaco-editor/react";
+
+const EDITOR_LOADING_STATE = {
+  value: "loading...",
+  language: "javascript"
+};
 
 export default function Home() {
+  const [gameFile, setGameFile] = useState(null);
+  const [testFile, setTestFile] = useState(null);
+  const [activeFileName, changeActiveFileName] = useState("game.js");
+  const [editor, setEditor] = useState(EDITOR_LOADING_STATE);
+
+  useEffect(() => {
+    const fetchGameFile = async () => {
+      const gameFile = await fetch("/game.js");
+      setGameFile(await gameFile.text());
+    };
+    fetchGameFile();
+  }, []);
+
+  useEffect(() => {
+    const fetchTestFile = async () => {
+      const testFile = await fetch("/tests.js");
+      setTestFile(await testFile.text());
+    };
+    fetchTestFile();
+  }, []);
+
+  useEffect(() => {
+    switch (activeFileName) {
+      case "game.js":
+        if (gameFile)
+          setEditor({
+            path: "game.js",
+            language: "javascript",
+            value: gameFile
+          });
+        else setEditor(EDITOR_LOADING_STATE);
+        break;
+      case "tests.js":
+        if (testFile)
+          setEditor({
+            path: "tests.js",
+            language: "javascript",
+            value: testFile
+          });
+        else setEditor(EDITOR_LOADING_STATE);
+        break;
+    }
+  }, [activeFileName, gameFile, testFile]);
+
+  const handleFileSelectChange = e => changeActiveFileName(e.target.value);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>IDE Spike</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <select value={activeFileName} onChange={handleFileSelectChange}>
+          <option value="game.js">game.js</option>
+          <option value="tests.js">tests.js</option>
+        </select>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <Editor
+          path={editor.path}
+          defaultLanguage={editor.language}
+          defaultValue={editor.value}
+        />
       </main>
 
       <footer className={styles.footer}>
@@ -56,10 +81,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
